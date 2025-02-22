0dfdaa659a90c71e5cbeeecc9ca17a8c2c841177
Sergi <128138115+SergiSalas@users.noreply.github.com>
2025-02-22 13:12:01 +0100

    CreationOfTheProject

---
+++ BackendTFG/src/test/java/com/tecnocampus/backendtfg/BackendTfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnocampus.backendtfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
